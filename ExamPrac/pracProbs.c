0e6fa31e240dd432421fd04b0a7d483ec796ff57
MorganGravelat <mgravelat@gmail.com>
2024-04-18 20:46:42 -0400

    Setup for pracProb.c file

---
+++ ExamPrac/pracProbs.c
@@ -0,0 +1,18 @@
+/*
+Anisa and Ben are trying to pick a name for their child. Based on their review of pseudoscience,
+they’ve decided that they want the shortest name possible. If there are multiple shortest
+names, they want the one that comes first alphabetically. Write a function that takes in a list of
+possible names (type char**) and returns the name (char*) that they should choose amongst the
+possible names in the input array, according to their criteria. Please use the appropriate functions
+in string.h and do NOT allocate memory, free memory, OR print anything. (You’ll get an
+automatic 0 if you do any of these things.) You may assume that the strings stored in
+possibleNames only consist of lowercase letters. (Note: numNames represents the number of
+names stored in the array possibleNames.)
+*/
+
+#include <string.h>
+char* getBestName(char** possibleNames, int numNames) {
+
+    
+
+}
